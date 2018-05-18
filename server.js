81f15a75d92cbde1a9607d33afa9842cd67551fd
arindam-modak <arindam.modak03@gmail.com>
2018-05-18 11:17:43 +0530

    google auth complete collection made

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(session({ secret: 'onetwothree' })); // session secret
-//app.use(passport.initialize());
-//app.use(passport.session()); // persistent login sessions
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
