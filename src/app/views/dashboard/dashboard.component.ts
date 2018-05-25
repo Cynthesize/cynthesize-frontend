f5c98bd18f6249015308b3389f26b3229d6aa292
arindam-modak <arindam.modak03@gmail.com>
2018-05-25 15:40:04 +0530

    firebase angular integrated and dashboard

---
+++ src/app/views/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
