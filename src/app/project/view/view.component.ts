011927dcf10ac7d2a97b05012e81e4b38a82e9ec
Aayush <iit2017017@iiita.ac.in>
2018-12-27 13:54:37 +0530

    Project View Designed

---
+++ src/app/project/view/view.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { IssueComponent } from '../issue/issue.component';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
