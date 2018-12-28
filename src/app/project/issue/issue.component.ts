74350dd01f49e2c9b86b577a47e7a48b6eb5bb84
Vivek Patel <vivekjasubhai4@gmail.com>
2018-12-28 18:06:29 +0530

    Added froala editor in issue section

--- src/app/project/issue/issue.component.ts
+++ src/app/project/issue/issue.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class IssueComponent implements OnInit {
+  public editorContent: string = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facilis similique odit aliquid eligendi laboriosam
+  repudiandae quam veritatis, harum officia, ratione neque quas natus quibusdam alias hic, id libero doloribus! Lorem
+  ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsa nostrum odio est ea, nulla pariatur. Laudantium
+  totam eius repudiandae iste, saepe illum commodi quidem suscipit architecto aspernatur, quibusdam labore.`;
   constructor() {}
 
   ngOnInit() {}
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -5,12 +5,22 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { AddComponent } from './add/add.component';
 import { SharedModule } from '@app/shared';
 import { MaterialModule } from '@app/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ViewComponent } from './view/view.component';
 import { IssueComponent } from './issue/issue.component';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 @NgModule({
   declarations: [AddComponent, ViewComponent, IssueComponent],
-  imports: [CommonModule, ProjectRoutingModule, SharedModule, MaterialModule, ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    ProjectRoutingModule,
+    SharedModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot()
+  ]
 })
 export class ProjectModule {}
