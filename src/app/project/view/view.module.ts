7e0863dbfc4c6bbf3e088bcdff3a69c0406ac979
Siddhant Srivastav <harshsrivastav123@gmail.com>
2019-01-15 22:18:13 +0530

    Fixes import issues (#33)
    * refactored issue component
    
    * Added user cotributions
    
    * Added profile picture upload facility
    
    * Fixed errors
    
    * Fixes import issues

--- src/app/project/view/view.module.ts
+++ src/app/project/view/view.module.ts
@@ -5,13 +5,23 @@ import { ViewRoutingModule } from './view-routing.module';
 import { ViewComponent } from './view.component';
 import { SharedModule } from '@app/shared';
 import { MaterialModule } from '@app/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { IssueComponent } from './issue/issue.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { IssueComponent, AddIssueComponent } from './issue/issue.component';
 import { DetachedIssueComponent } from './issue/detached-issue/detached-issue.component';
 import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentTextEditorModule } from '@covalent/text-editor';
 
 @NgModule({
-  declarations: [ViewComponent, IssueComponent, DetachedIssueComponent],
-  imports: [CommonModule, ViewRoutingModule, SharedModule, MaterialModule, ReactiveFormsModule, CovalentMarkdownModule]
+  declarations: [ViewComponent, IssueComponent, DetachedIssueComponent, AddIssueComponent],
+  imports: [
+    CommonModule,
+    ViewRoutingModule,
+    SharedModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    CovalentMarkdownModule,
+    CovalentTextEditorModule,
+    FormsModule
+  ]
 })
 export class ViewModule {}
