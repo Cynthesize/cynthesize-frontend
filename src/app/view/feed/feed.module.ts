5368a4805e71345caa6fb61da0a131d2581115c7
WickedBrat <harshsrivastav123@gmail.com>
2019-01-31 03:03:47 +0530

    Adds idea display modal

--- src/app/view/feed/feed.module.ts
+++ src/app/view/feed/feed.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedRoutingModule } from './feed-routing.module';
-import { IdeaFeedComponent } from './idea-feed/idea-feed.component';
+import { IdeaFeedComponent, IdeaCardComponent } from './idea-feed/idea-feed.component';
 import { IssuesFeedComponent } from './issues-feed/issues-feed.component';
-import { IdeaCardComponent } from './idea-feed/idea-card/idea-card.component';
 import { SharedModule } from '@app/shared';
 import { MaterialModule } from '@app/material.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [IdeaFeedComponent, IssuesFeedComponent, IdeaCardComponent],
-  imports: [CommonModule, FeedRoutingModule, SharedModule, MaterialModule, NgxPaginationModule]
+  imports: [CommonModule, FeedRoutingModule, SharedModule, MaterialModule, NgxPaginationModule],
+  entryComponents: [IdeaCardComponent]
 })
 export class FeedModule {}
--- src/app/view/feed/idea-feed/idea-feed.component.ts
+++ src/app/view/feed/idea-feed/idea-feed.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { IdeaService } from '@app/core/idea/idea.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-idea-feed',
@@ -9,7 +10,16 @@ import { IdeaService } from '@app/core/idea/idea.service';
 export class IdeaFeedComponent implements OnInit {
   length: number;
   ideaList: any;
-  constructor(private ideaService: IdeaService) {}
+  constructor(private ideaService: IdeaService, public dialog: MatDialog) {}
+
+  openDialog(idea: any): void {
+    const dialogRef = this.dialog.open(IdeaCardComponent, {
+      width: 'auto',
+      data: { idea }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {});
+  }
 
   ngOnInit() {
     this.ideaService.getTotalIdeaCount().subscribe(data => {
@@ -22,5 +32,17 @@ export class IdeaFeedComponent implements OnInit {
     this.ideaService.getNIdeas(event.pageSize, event.pageIndex).subscribe(data => {
       this.ideaList = data.data.ideas;
     });
+  }
+}
+@Component({
+  selector: 'app-idea-card',
+  templateUrl: './idea-card/idea-card.component.html',
+  styleUrls: ['./idea-card/idea-card.component.scss']
+})
+export class IdeaCardComponent {
+  constructor(public dialogRef: MatDialogRef<IdeaCardComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
