66a58ae60272c9ef7b2477c63b40dc5eb936d1f4
Prijesh <prijeshbhingradiya1@gmail.com>
2019-02-13 20:48:51 +0530

    Added nav-bar-image to right

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -4,13 +4,30 @@ import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@app/material.module';
-
+import { Cloudinary as CloudinaryCore } from 'cloudinary-core';
+import { CloudinaryConfiguration, CloudinaryModule } from '@cloudinary/angular-5.x';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
 import { GraphqlModule } from './graphql/graphql.module';
 
+export const cloudinary = {
+  Cloudinary: CloudinaryCore
+};
+export const config: CloudinaryConfiguration = {
+  cloud_name: 'cynthesize',
+  upload_preset: 'qdninpjl'
+};
+
 @NgModule({
-  imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, RouterModule, GraphqlModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    FlexLayoutModule,
+    MaterialModule,
+    RouterModule,
+    GraphqlModule,
+    CloudinaryModule.forRoot(cloudinary, config)
+  ],
   declarations: [HeaderComponent, ShellComponent]
 })
 export class ShellModule {}
