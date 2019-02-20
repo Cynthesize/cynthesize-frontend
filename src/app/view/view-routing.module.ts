5d9a09944d4dd1356e171285d8a383f6de359d8d
Sanyam Dogra <sdlord07@gmail.com>
2019-02-20 12:01:45 +0530

    Reflecting changes in URL according to active checkpoint (#139)
    * Added 'name' as a parameter
    
    * Added checkpoint routing
    
    * Corrected routing paramter
    
    * Corrected Checkpoints data
    
    * Imporove Routing
    
    * Removed navigator and used routerLink
    
    * Added styling
    
    * Made the requested changes
    
    * removes console log

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewProjectModule } from './view-project/view-project.module';
 
 const routes: Routes = [
   {
     path: 'project/:id',
+    loadChildren: 'app/view/view-project/view-project.module#ViewProjectModule'
+  },
+  {
+    path: 'project/:id/:name',
     loadChildren: 'app/view/view-project/view-project.module#ViewProjectModule'
   },
   {
