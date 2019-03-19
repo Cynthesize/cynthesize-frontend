021beeb17d47cdc2367386e8878bd98ac2aff6ab
WickedBrat <harshsrivastav123@gmail.com>
2019-03-20 01:03:23 +0530

    Fix environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,10 +8,10 @@ export const environment = {
   supportedLanguages: ['en-US', 'fr-FR']
 };
 
-export const HASURA_ACCESS_KEY = 'top_secret_key';
-export const GRAPHQL_URL = 'https://cynth-87hdf545mhgfg.herokuapp.com/v1alpha1/graphql';
-export const REALTIME_GRAPHQL_URL = 'wss://cynth-87hdf545mhgfg.herokuapp.com/v1alpha1/graphql';
+export const HASURA_ACCESS_KEY = 'secret';
+export const GRAPHQL_URL = 'https://cynthesize-back.herokuapp.com/v1alpha1/graphql';
+export const REALTIME_GRAPHQL_URL = 'wss://cynthesize-back.herokuapp.com/v1alpha1/graphql';
 export const authClientId = 'tEbTH2wRl3dHtWZ0m4nqoWPsN1GIu9dQ';
 export const authDomain = 'cynthesize.auth0.com';
 export const auth0Audience = 'https://cynthesize.auth0.com/userinfo';
-export const callbackUrl = 'http://cynthesize-develop.netlify.com/callback';
+export const callbackUrl = 'http://localhost:4200/callback';
