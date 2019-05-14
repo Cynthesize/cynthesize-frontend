26a3277b31fc16fdd12d5db232fcfa635c492498
WickedBrat <harshsrivastav123@gmail.com>
2019-05-14 10:38:57 +0530

    deplot script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 read -p "Enter the name of branch you want to deploy: " branch_name
+git reset --hard
 git checkout $branch_name
 git pull origin $branch_name
 npm install -g @angular/cli
+
+echo "Removing development index.html..."
+rm cynthesize-frontend/index.html
+echo "Copying production index.html..."
+cp ../index.html ./
+
 ng build --prod --aot
 cd ..
 rm -rf cynthesize/dist
 echo "Moving files to host distribution folder..."
 mv cynthesize-frontend/dist cynthesize
-echo "Removing development index.html..."
-rm cynthesize/dist/index.html
-echo "Copying production index.html..."
-cp index.html cynthesize/dist
 cd cynthesize
 gcloud app deploy
