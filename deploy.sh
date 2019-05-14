dbdfd5d464d341d23b7a14e3c37d3fc9f96c40fa
WickedBrat <harshsrivastav123@gmail.com>
2019-05-14 09:59:11 +0530

    Deploy file changes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 read -p "Enter the name of branch you want to deploy: " branch_name
+git checkout $branch_name
 git pull origin $branch_name
 npm install -g @angular/cli
 ng build --prod --aot
 cd ..
 rm -rf cynthesize/dist
+echo "Moving files to host distribution folder..."
 mv cynthesize-frontend/dist cynthesize
 cd cynthesize
 gcloud app deploy
