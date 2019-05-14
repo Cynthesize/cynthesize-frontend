2bdb76cba758d06e90c4a3a175f5795b1aa00f6f
WickedBrat <harshsrivastav123@gmail.com>
2019-05-14 10:05:25 +0530

    Deploy file changes

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ cd ..
 rm -rf cynthesize/dist
 echo "Moving files to host distribution folder..."
 mv cynthesize-frontend/dist cynthesize
+echo "Removing development index.html..."
+rm cynthesize/dist/index.html
+echo "Copying production index.html..."
+cp index.html cynthesize/dist
 cd cynthesize
 gcloud app deploy
