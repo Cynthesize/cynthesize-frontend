a49203c7cbef560e694635299da05abf8a49dd46
WickedBrat <harshsrivastav123@gmail.com>
2019-05-15 07:41:29 +0530

    changed default home route

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,10 +6,7 @@ import { HomeComponent } from './home.component';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
-  Shell.childRoutes([
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, data: { title: extract('Home') } }
-  ])
+  Shell.childRoutes([{ path: '', component: HomeComponent, data: { title: extract('Cynthesize | Home') } }])
 ];
 
 @NgModule({
